0fa5a439c8ac88cef24a0856e47cde5239e120a1
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-02 08:53:54 -0300

    add connect button

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,25 @@
 import { ethers } from "./ethers-5.8.0.esm.min.js";
 
+const root = document.getElementById("root");
 console.log(ethers);
+
+const connectHandler = async () => {
+  try {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const app = () => {
+  if (typeof window.ethereum !== "undefined") {
+    root.innerHTML = `<button id="connectBtn">connect</button>`;
+    document
+      .querySelector("#connectBtn")
+      .addEventListener("click", connectHandler);
+  } else {
+    root.innerHTML = `<h2 style="color:red;">Please install Metamask!</h2>`;
+  }
+};
+
+app();
